6798b3ff1e2fbd16bd70872ad25f6b5d49fb0368
reich452 <reich452@regis.edu>
2017-03-25 16:06:53 -0600

    Added DeckadeController

--- capstoneTest2/DeckadeController.swift
+++ capstoneTest2/DeckadeController.swift
@@ -10,6 +10,36 @@ import Foundation
 
 class DeckadeController {
     
+    static let baseUrl = URL(string: "https://api.cognitive.microsoft.com/bing/v5.0/images/search?")
+    static let apiKey = "8a4c9444f22e4dc1b0828c309e4280e9"
     
-    
+    static func fetchMovie(for deckadeSearch: String, completion: @escaping ([Deckade]) -> Void) {
+        
+        guard let baseUrl = baseUrl else { completion([]); return }
+        
+        let urlParameters = ["q": deckadeSearch, "Ocp-Apim-Subscription-Key": apiKey]
+        
+        NetworkController.performRequest(for: baseUrl, httpMethod: .Get, urlParameters: urlParameters, body: nil) { (data, error) in
+            
+            if let error = error {
+                print(error.localizedDescription)
+                completion([])
+                return
+            }
+            
+            guard let data = data,
+                let jsonDictionaries = (try? JSONSerialization.jsonObject(with: data, options: .allowFragments)) as? [String: Any] else { completion([]); return }
+            
+            guard let deckadeArray = jsonDictionaries["value"] as? [[String: Any]] else { completion([]); return }
+            
+            let deckades = deckadeArray.flatMap( {Deckade(jsonDictionary: $0)})
+            
+            for deckade in deckades {
+                ImageController.image(forURL: deckade.contentUrlString, completion: { (newImage) in
+                    
+                })
+            }
+            completion(deckades)
+        }
+    }
 }
